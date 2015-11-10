d1be8aa3f63fe3f0023c4aad4ff2e19d31dc5c69
Gauthier Lamaison <gauthier.lamaison@gmail.com>
2015-11-10 17:25:16 +0000

    Loading configuration

--- emacs/init.el
+++ emacs/init.el
@@ -1,32 +1,26 @@
 (setq user-init-file (or load-file-name (buffer-file-name)))
 (setq user-emacs-directory (file-name-directory user-init-file))
 
-;; (package-initialize) ;; You might already have this line
+(require 'cask (concat (file-name-as-directory (getenv "CONF_HOME")) (concat (file-name-as-directory "cask") "cask.el")))
+(cask-initialize)
 
-;; ;;; Recursively Load Path
-;; (let ((default-directory (expand-file-name (concat user-emacs-directory (convert-standard-filename "nonpackaged/")))))
-;;   (setq load-path
-;; 	(append
-;; 	 (let ((load-path (copy-sequence load-path))) ;; Shadow
-;; 	   (append
-;; 	    (copy-sequence (normal-top-level-add-to-load-path '(".")))
-;; 	    (normal-top-level-add-subdirs-to-load-path)))
-;; 	 load-path)))
-;; (setq load-path (cons (concat user-emacs-directory (convert-standard-filename "conf/")) load-path))
+;;; Recursively Load Path
+(let ((default-directory (expand-file-name (concat user-emacs-directory (convert-standard-filename "nonpackaged/")))))
+  (setq load-path
+	(append
+	 (let ((load-path (copy-sequence load-path))) ;; Shadow
+	   (append
+	    (copy-sequence (normal-top-level-add-to-load-path '(".")))
+	    (normal-top-level-add-subdirs-to-load-path)))
+	 load-path)))
+(setq load-path (cons (concat user-emacs-directory (convert-standard-filename "conf/")) load-path))
 
-;; ;;; configuration
-;; (load "requires")
-;; (load "c-lang")
-;; (load "golang")
-;; (load "tmux")
-;; (load "ui")
-;; (load "editor")
-
-;; ;;; MELPA
-;; (add-to-list 'package-archives
-;;              '("melpa" . "http://melpa.org/packages/") t)
-;; (when (< emacs-major-version 24)
-;;   ;; For important compatibility libraries like cl-lib
-;;   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;;; configuration
+(load "requires")
+(load "c-lang")
+(load "golang")
+(load "tmux")
+(load "ui")
+(load "editor")
 
 (print "Configuration loaded")
