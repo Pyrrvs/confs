ea32a01f4d9b6c556d037b9d0971d8607eee28fc
Gauthier Lamaison <gauthier.lamaison@gmail.com>
2015-11-10 17:28:48 +0000

    removing python for emacs

--- emacs/conf/requires.el
+++ emacs/conf/requires.el
@@ -1,10 +1,3 @@
-(require 'package)
-(require 'epy-setup)      ;; It will setup other loads, it is required!
-(require 'epy-python)     ;; If you want the python facilities [optional]
-(require 'epy-completion) ;; If you want the autocompletion settings [optional]
-(require 'epy-editing)    ;; For configurations related to editing [optional]
-(require 'epy-bindings)   ;; For my suggested keybindings [optional]
-(require 'epy-nose)       ;; For nose integration
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (require 'ac-c-headers)
